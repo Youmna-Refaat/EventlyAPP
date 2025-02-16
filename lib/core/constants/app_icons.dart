e72aad2ce5b0c70db31cb9f8b070eff2c4bf08b3
Youmna-Refaat <youmnarefaat884@gmail.com>
2025-02-16 14:51:57 +0200

    core: costants

---
+++ lib/core/constants/app_icons.dart
@@ -0,0 +1,27 @@
+abstract class AppIcons {
+  static const String iconsArrow = "assets/icons/arrow.png";
+  static const String iconsBackArrow = "assets/icons/backArrow.png";
+  static const String iconsAll = "assets/icons/all.svg";
+  static const String iconsArabic = "assets/icons/arabic.svg";
+  static const String iconsEmail = "assets/icons/email.svg";
+  static const String iconsEnglish = "assets/icons/english.svg";
+  static const String iconsGoogle = "assets/icons/google.png";
+  static const String iconsHome = "assets/icons/home.svg";
+  static const String iconsLock = "assets/icons/lock.svg";
+  static const String iconsPerson = "assets/icons/person.svg";
+  static const String iconsLightTheme = "assets/icons/light.svg";
+  static const String iconsDarkTheme = "assets/icons/dark.svg";
+  static const String iconsEventLocation = "assets/icons/eventLocation.svg";
+  static const String iconsEdit = "assets/icons/edit.svg";
+  static const String iconsDelete = "assets/icons/delete.svg";
+  static const String iconsCalender = "assets/icons/calender.svg";
+  static const String iconsSearch = "assets/icons/Search.svg";
+  static const String iconsMeeting = "assets/icons/meeting.svg";
+  static const String iconsHoliday = "assets/icons/holiday.svg";
+  static const String iconsWorkshop = "assets/icons/workshop.svg";
+  static const String iconsSport = "assets/icons/sport.svg";
+  static const String iconsBirthday = "assets/icons/birthday.svg";
+  static const String iconsBookClub = "assets/icons/bookClub.svg";
+  static const String iconsGaming = "assets/icons/gaming.svg";
+  static const String iconsExihbition = "assets/icons/Exhibition.svg";
+}
---
+++ lib/core/constants/app_images.dart
@@ -0,0 +1,23 @@
+abstract class AppImages {
+  static const String imagesForgetPassword = "assets/images/forgetPassword.png";
+  static const String imagesLogo = "assets/images/logo.png";
+  static const String imagesMeeting = "assets/images/meetings.png";
+  static const String imagesGaming = "assets/images/gaming.png";
+  static const String imagesWorkshop = "assets/images/workshop.png";
+  static const String imagesSport = "assets/images/sports.png";
+  static const String imagesHoliday = "assets/images/Holidays.png";
+  static const String imagesBirthday = "assets/images/Birthdays.png";
+  static const String imagesBookClub = "assets/images/Book Club.png";
+  static const String imagesExhibition = "assets/images/Exhibtions.png";
+  static const String imagesOnBoardingAppBar =
+      "assets/images/onBoardingAppBar.png";
+  static const String imagesOnBoardingExperience =
+      "assets/images/onBoardingExperience.png";
+  static const String imagesOnBoardingInspire =
+      "assets/images/onBoardingInspire.png";
+  static const String imagesOnBoardingPlanning =
+      "assets/images/onBoardingPlanning.png";
+  static const String imagesOnBoardingShare =
+      "assets/images/onBoardingShare.png";
+  static const String imagesProfile = "assets/images/profile.png";
+}
